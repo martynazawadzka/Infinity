339eaff746e89946dfe352436244f78b3b945bb2
martynazawadzka <martyska94@wp.pl>
2018-08-03 15:50:43 +0200

    Change let for const if possible

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,21 @@
 function showMenu() {
-    let navElement = document.querySelector('.nav');
+    const navElement = document.querySelector('.nav');
     navElement.classList.add('nav--overlay');
 }
 
 function hideMenu() {
-    let navElement = document.querySelector('.nav');
+    const navElement = document.querySelector('.nav');
     navElement.classList.remove('nav--overlay');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    let closeElement = document.querySelector('.nav__close-button');
+    const closeElement = document.querySelector('.nav__close-button');
     closeElement.addEventListener('click', hideMenu);
 
-    let menuElement = document.querySelector('.nav__menu');
+    const menuElement = document.querySelector('.nav__menu');
     menuElement.addEventListener('click', showMenu);
 
-    let menuItems = document.querySelectorAll('.nav__item');
+    const menuItems = document.querySelectorAll('.nav__item');
     menuItems.forEach(element => {
         element.addEventListener('click', hideMenu);
     });
--- js/products.js
+++ js/products.js
@@ -1,31 +1,31 @@
 "use strict"
 
 function showPopup(imageUrl, itemText) {
-    let popupElement = document.querySelector('.popup__content');
+    const popupElement = document.querySelector('.popup__content');
     popupElement.classList.add('popup__content--active');
 
-    let popupImage = document.querySelector('.popup__img');
+    const popupImage = document.querySelector('.popup__img');
     popupImage.setAttribute('src', imageUrl);
 
-    let popupText = document.querySelector('.popup__text');
+    const popupText = document.querySelector('.popup__text');
     popupText.innerHTML = itemText;
 }
 
 function hidePopup() {
-    let popupElement = document.querySelector('.popup__content');
+    const popupElement = document.querySelector('.popup__content');
     popupElement.classList.remove('popup__content--active');
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    let popup = document.querySelector('.products__popup');
+    const popup = document.querySelector('.products__popup');
     popup.addEventListener('click', hidePopup);
 
 
-    let itemList = document.querySelectorAll('.items__item');
+    const itemList = document.querySelectorAll('.items__item');
     for (let i = 0; i < itemList.length; i++) {
         itemList[i].addEventListener('click', function () {
-            let imageUrl = this.querySelector('.items__img').getAttribute('src');
-            let itemText = this.querySelector('.items__description').innerHTML;
+            const imageUrl = this.querySelector('.items__img').getAttribute('src');
+            const itemText = this.querySelector('.items__description').innerHTML;
 
             showPopup(imageUrl, itemText);
         });
